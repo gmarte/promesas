4a3f0a6ebd5068b0be52221582918617140a0469
gmarte <giancarlo.marte@gmail.com>
2022-09-27 22:45:44 -0400

    initial set up of models and templates

--- promise_tracker/models.py
+++ promise_tracker/models.py
@@ -1,3 +1,23 @@
+from pyexpat import model
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class User(AbstractUser):
+    pass
+class Politician(models.Model):
+    pass
+class Party(models.Model):
+    class Meta:
+        verbose_name = "Party"
+        verbose_name_plural = "Parties"
+class Promise(models.Model):
+    pass
+class Position(models.Model):
+    pass
+class Evidence(models.Model):
+    pass
+class Source(models.Model):
+    pass
+
