eaec5c073a36ace1e1514e2afef2b3917a6d7dad
Giancarlo Marte <giancarlo.marte@gmail.com>
2022-10-03 15:19:26 -0400

    testing commit

--- promise_tracker/models.py
+++ promise_tracker/models.py
@@ -1,4 +1,3 @@
-from pyexpat import model
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils import timezone
@@ -6,18 +5,29 @@ from django.utils import timezone
 
 class User(AbstractUser):
     pass
+
+
 class Politician(models.Model):
     pass
+
+
 class Party(models.Model):
     class Meta:
         verbose_name = "Party"
         verbose_name_plural = "Parties"
+
+
 class Promise(models.Model):
     pass
+
+
 class Position(models.Model):
     pass
+
+
 class Evidence(models.Model):
     pass
+
+
 class Source(models.Model):
     pass
-
