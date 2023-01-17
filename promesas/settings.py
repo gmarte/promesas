612e7fc018a7ee4d8b2ed18e4ee7038ec37d389d
Giancarlo Marte <giancarlo.marte@gmail.com>
2023-01-17 18:21:52 -0400

    add allow host for debug mode

--- promesas/settings.py
+++ promesas/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = str(os.environ.get('DEBUG')) == '1'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 if not DEBUG:
     ALLOWED_HOSTS += os.environ.get('ALLOWED_HOSTS')
 
