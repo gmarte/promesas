4ce62afbd9957b4f02de4a656fe43b51e735c244
Giancarlo Marte <giancarlo.marte@gmail.com>
2023-01-17 17:21:43 -0400

    adding build.sh

--- promesas/settings.py
+++ promesas/settings.py
@@ -140,9 +140,13 @@ USE_TZ = True
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'promise_tracker/static')
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR.child('promise_tracker'), "static"),
-# ]
+# Following settings only make sense on production and may break development environments.
+if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
+    # in your application directory on Render.
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Turn on WhiteNoise storage backend that takes care of compressing static files
+    # and creating unique names for each version so they can safely be cached forever.
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
